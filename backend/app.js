dfdb4bd74bfb9e0114fb046c205563c971745dd6
emmanyouwell <emingala02@gmail.com>
2024-10-19 16:39:20 +0800

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -6,16 +6,18 @@ const group = require('./routes/group');
 const auth = require('./routes/auth');
 const subtopic = require('./routes/subtopic');
 const quiz = require('./routes/quiz');
-app.use(cors(
-    {
-        origin: ["http://localhost:5173", "https://it-elective-3-project.vercel.app","https://tuphanda.onrender.com", "https://tup-handa.vercel.app", "https://www.youtube.com"],
-        credentials: true
-    }
-))
+// CORS configuration
+const corsOptions = {
+    origin: 'https://it-elective-3-project.vercel.app', // Allow only your Vercel frontend
+    methods: 'GET,POST,PUT,DELETE', // Allow specific methods if needed
+    credentials: true, // If your frontend requires credentials (e.g., cookies)
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({limit: '50mb', extended: true}))  
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookie());
-app.use('/api/v1',group);
+app.use('/api/v1', group);
 app.use('/api/v1', auth);
 app.use('/api/v1', subtopic);
 app.use('/api/v1', quiz);
